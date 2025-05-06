ad7fe84286e5cacd470ccbe2c67eb40dbec023b4
waku <wakunguma13@gmail.com>
2025-05-06 10:20:21 -0600

    feat: add auth and api modules

---
+++ src/api.rs
@@ -0,0 +1,13 @@
+
+
+pub async fn sign_up() -> &'static str{
+    ""
+}
+
+pub async fn sign_in(){
+
+}
+
+pub async fn log_out(){
+
+}
---
+++ src/auth.rs
@@ -0,0 +1,181 @@
+use std::time;
+use base64::engine::{general_purpose::URL_SAFE,Engine};
+use chrono::{DateTime, Duration, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx::{prelude::FromRow, PgPool};
+use uuid::Uuid;
+
+#[derive(Debug,Serialize,Deserialize,FromRow)]
+pub struct User{
+    pub id: Uuid,
+    pub email: Option<String>,
+    pub user_name: Option<String>
+}
+
+#[derive(Debug,Serialize,Deserialize,FromRow)]
+pub struct RefreshToken{
+    pub id: Uuid,
+    pub token: String,
+    pub created_at: DateTime<Utc>,
+    pub session_id: Uuid,
+    pub active: bool,
+}
+
+#[derive(Debug,Serialize,Deserialize,FromRow,PartialEq, Eq)]
+pub struct SessionToken{
+    pub id: Uuid,
+    pub token: String,
+    pub created_at: DateTime<Utc>,
+    pub expires_at: DateTime<Utc>,
+    pub user_id: Uuid,
+}
+
+pub async fn check_user_auth(){
+
+}
+
+/// Create a 32 byte string filled with
+/// random data.
+pub fn create_token() -> String{
+    let bytes:[u8;32] = rand::random();
+    URL_SAFE.encode(bytes)
+}
+
+
+/// Create a session token and insert it 
+/// into the database.
+pub async fn create_session_token(pool: &PgPool) -> crate::Result<SessionToken>{
+    let session_token = create_token();
+    let expiry = Utc::now() + time::Duration::from_secs(60 * 30);
+    
+    let token = 
+        sqlx::query_as::<_,SessionToken>(
+            "INSERT INTO session_tokens(token,expires_at) VALUES($1,$2) RETURNING *"
+        )
+        .bind(session_token)
+        .bind(expiry)
+        .fetch_one(pool)
+        .await?;
+    Ok(token)
+}
+
+pub async fn create_refresh_token(){
+
+}
+
+pub async fn create_token_pair(){
+
+}
+
+/// Refresh the user tokens
+pub async fn refresh_tokens(){
+
+}
+
+/// Create a user in the database
+pub async fn create_user(pool: &PgPool,desc: UserDesc) -> crate::Result<User>{
+    let instant = tokio::time::Instant::now();
+    let user = sqlx::query_as::<_,User>(
+        "INSERT INTO users(id) VALUES($1) RETURNING *",
+    )
+    .bind(desc.id)
+    .fetch_one(pool).await?;
+
+    let session_token = create_token();
+    let refresh_token = create_token();
+    let expiry = Utc::now() + time::Duration::from_secs(60 * 30);
+    
+    let session = 
+        sqlx::query_as::<_,SessionToken>(
+            "INSERT INTO session_tokens(token,expires_at) VALUES($1,$2) RETURNING *"
+        )
+        .bind(session_token)
+        .bind(expiry)
+        .fetch_one(pool)
+        .await?;
+
+    let refresh_token = 
+        sqlx::query_as::<_,RefreshToken>(
+            "
+            INSERT INTO refresh_tokens(token,session_id,active) 
+            VALUES($1,$2,true) 
+            RETURNING *
+            "
+        )
+        .bind(refresh_token)
+        .bind(session.id)
+        .fetch_one(pool)
+        .await?;
+
+    dbg!(session);
+    dbg!(refresh_token);
+
+    dbg!(instant.elapsed());
+    
+    Ok(user)
+}
+
+#[derive(Debug,Clone)]
+pub struct UserDesc{
+    id: Uuid,
+    email: Option<String>,
+    user_name: Option<String>
+}
+
+impl UserDesc{
+    pub fn new() -> Self{
+        Self { 
+            id: Uuid::new_v4(), 
+            email: None, 
+            user_name: None 
+        }
+    }
+
+    pub fn email(&mut self,email: &str){
+        self.email = Some(email.to_owned());
+    }
+
+    pub fn user_name(&mut self,user_name: &str){
+        self.user_name = Some(user_name.to_owned());
+    } 
+}
+
+
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[sqlx::test]
+    async fn new_session_token(pool: PgPool) -> crate::Result<()>{
+        let token = create_session_token(&pool).await?;
+        
+        let session_token = sqlx::query_as::<_,SessionToken>(
+            "SELECT * FROM session_tokens WHERE id = $1"
+        )
+        .bind(token.id)
+        .fetch_one(&pool)
+        .await?;
+
+        let max_expiry = Utc::now() + time::Duration::from_secs(60 * 30);
+
+        assert_eq!(token,session_token);
+        assert!(session_token.expires_at <= max_expiry);
+        Ok(())
+    }
+
+    #[sqlx::test]
+    async fn create_new_user(pool: PgPool) -> crate::Result<()>{
+        let mut user = UserDesc::new();
+        user.email("wakunguma13@gmail.com");
+        create_user(&pool, user).await?;
+
+        let result = sqlx::query_as::<_,User>("SELECT * FROM users")
+            .fetch_all(&pool)
+            .await?;
+
+        dbg!(result);
+
+        Ok(())
+    }
+}
