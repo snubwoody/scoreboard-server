809e383e2accb5f1fe34565e1c1634235b8e7c7a
waku <wakunguma13@gmail.com>
2025-05-06 21:20:22 -0600

    delete old migrations
    I'm doing everything again but actually designing first this time

--- migrations/20250506173525_add_default_constraints.sql
+++
@@ -1,9 +0,0 @@
--- Add migration script here
-
-ALTER TABLE session_tokens
-ALTER COLUMN created_at
-SET DEFAULT now();
-
-ALTER TABLE refresh_tokens
-ALTER COLUMN created_at
-SET DEFAULT now();
--- migrations/20250506174615_alter_users_table.sql
+++
@@ -1,4 +0,0 @@
--- Add migration script here
-ALTER TABLE users
-ADD COLUMN encrypted_password TEXT NULL,
-ADD COLUMN created_at TIMESTAMPTZ NOT NULL DEFAULT now();
